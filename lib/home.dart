872464f1be8bd0b346604ae11161d781825e3d83
Muitsu <blankdahma@gmail.com>
2024-12-10 11:23:26 +0800

    remove

--- lib/home.dart
+++ lib/home.dart
@@ -15,7 +15,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(title: const Text("Re-Captcha Apps")),
       body: Column(
-        children: <Widget>[
+        children: [
           ElevatedButton(
               onPressed: () {
                 CaptachaDialog.show(
